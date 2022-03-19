ea342bb730904a9072324d9b5c51117a5f3d12a0
Tesh2714 <teas38mum@gmail.com>
2022-03-19 05:24:54 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
+/*
+ *  * File: 4-puts.c
+ *  *    */
+
 #include <stdio.h>
 
+/**
+ *  * main - Prints "Programming is like building a multilingual
+ *   * Return: Always 0.
+ *      */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
