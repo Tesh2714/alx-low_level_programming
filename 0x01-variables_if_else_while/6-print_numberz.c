371d4a132166fc7c166bd6e020984dc53ab6888f
Tesh2714 <teas38mum@gmail.com>
2022-03-19 20:16:50 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,15 +2,23 @@
 #include <time.h>
 #include <stdio.h>
 
+/* betty style doc for function main goes there */
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-	int i;
-	i = 0;
-	while  (i < 10)
-		{
-			putchar(i);
-			i++;
-		}
-	putchar('\n');
-	return (0);
+		int i;
+
+			i = 48;
+				while  (i < 58)
+						{
+									putchar(i);
+											i++;
+												}
+					putchar('\n');
+
+						return (0);
 }
