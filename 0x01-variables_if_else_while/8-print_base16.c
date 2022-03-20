b560e1825c5d0100799c39768aedf52f6e6ebdca
Tesh2714 <teas38mum@gmail.com>
2022-03-20 00:32:25 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,4 +1,4 @@
-include <stdio.h>
+#include <stdio.h>
 
 /**
  *   * main - Prints a hexadecimal string
@@ -7,19 +7,16 @@ include <stdio.h>
  *         */
 int main(void)
 {
-		char c;
+	char c;
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
 
-			for (c = '0'; c <= '9'; c++)
-					{
-								putchar(c);
-									}
-
-				for (c = 'a'; c <= 'f'; c++)
-						{
-									putchar(c);
-										}
-
-					putchar('\n');
-
-						return (0);
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
 }
