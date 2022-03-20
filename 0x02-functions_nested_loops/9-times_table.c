26a490865582d0362bc2423a4d8b5f91b3e7af5b
Tesh2714 <teas38mum@gmail.com>
2022-03-20 09:46:31 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,17 +6,16 @@
  *     */
 void times_table(void)
 {
-		int a, b, mult;
+	int a, b, mult;
 
-			for (a = 0 ; a <= 9 ; a++)
-					{
-								for(b = 0 ; b <= 9 ; b++)
-											for (b = 0 ; b <= 9 ; b++)
-														{
+	for (a = 0 ; a <= 9 ; a++)
+		{
+
+		for (b = 0 ; b <= 9 ; b++)
+		{
 																		mult = a * b;
 
-																					if(mult <= 9)
-																									if (mult <= 9)
+																		if (mult <= 9)
 																													{
 																																		if (b != 0)
 																																							{
