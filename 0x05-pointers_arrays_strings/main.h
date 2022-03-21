eb62d51915d2c5cf1067a0d17a19ca2d53021edb
Tesh2714 <teas38mum@gmail.com>
2022-03-21 12:48:27 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -11,3 +11,7 @@ void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
 void reset_to_98(int *n);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
