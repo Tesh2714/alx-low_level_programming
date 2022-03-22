cfdff2655211c906ca981171b91c40df13153425
Tesh2714 <teas38mum@gmail.com>
2022-03-21 21:52:22 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,24 @@
 #include "main.h"
+
 /**
- *  * puts2 - this function print only the even numbers.
- *   * @str: is the principal parameter.
- *    * Return: Always 0.
+ *  * puts2 - prints one char out of 2
+ *   * @str: type
+ *    *
  *     */
+
 void puts2(char *str)
 {
-		int i = 0;
+		int i, j = 0;
 
-			while (str[i] != 0)
+			while (str[i] != '\0')
 					{
-								if ((str[i] % 2) == 0)
-											if ((i % 2) == 0)
-															_putchar(str[i]);
-										i++;
-											}
-				_putchar('\n');
+								i++;
+									}
+
+				while (j < i)
+						{
+									_putchar(str[j]);
+											j = j + 2;
+												}
+					_putchar('\n');
 }
