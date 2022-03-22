adffbdedba1b5f1e6bf81c8c9eba8cecfdd3b9f7
Tesh2714 <teas38mum@gmail.com>
2022-03-22 16:14:43 -0700

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -11,3 +13,4 @@ void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
 void positive_or_negative(int i);
+#endif /* MAIN_H */
