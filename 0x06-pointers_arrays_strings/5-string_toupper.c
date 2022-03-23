270e103ffece427fa2d3e3a52374a3f9d6325e56
Tesh2714 <teas38mum@gmail.com>
2022-03-23 11:39:15 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,21 +2,6 @@
 #include <stdio.h>
 
 /**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
- *     */
-int main(void)
-{
-	    char str[] = "Look up!\n";
-	        char *ptr;
-
-		    ptr = string_toupper(str);
-		        printf("%s", ptr);
-			    printf("%s", str);
-			        return (0);
-}
-/**
  *  * string_toupper - changes all lowercase letters of a string to uppercase
  *   * @s: string to be capitalized
  *    *
