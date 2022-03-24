0b7a7d46bf59a9d2c327b950b3ad84dc24bce7f5
Tesh2714 <teas38mum@gmail.com>
2022-03-24 00:24:43 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * * mul - multiplies 123
+ * * @a: parameter
+ * * @b: parameter
+ * * Return: Returns 0
+ * */
+int mul(int a, int b)
+{
+		int product = a * b;
+
+			return (product);
+}
