9d5f49bcb839c589f8b125f643f85c7a06efd1cb
Tesh2714 <teas38mum@gmail.com>
2022-04-06 00:56:14 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,12 +4,12 @@
 
 /* betty style doc for function main goes there */
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ * *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		printf("0123456789\n");
-			return (0);
+	printf("0123456789\n");
+	return (0);
 }
