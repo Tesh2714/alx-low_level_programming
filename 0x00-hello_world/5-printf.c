a7845f3d31b2e2a29d5f03a58cf974224a706df9
Tesh2714 <teas38mum@gmail.com>
2022-04-05 23:35:19 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,18 +1,12 @@
-/*
- *  * File: 5-printf.c
- *   * Auth: Tesh2714
- *   *    */
-
 #include <stdio.h>
 
 /**
- *  * main - Prints 
- *    *
- *     * Return: Always 0.
- *      */
+* main:Prints text exactly with proper grammar
+* Return:Always 0.
+*/
+
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
