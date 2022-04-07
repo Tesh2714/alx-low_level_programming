def3e4ba6a0aed9ef0af21f8acc1072568db7560
Tesh2714 <teas38mum@gmail.com>
2022-04-07 05:53:33 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,7 +5,7 @@
  * malloc_checked - to allocates memory using malloc
  * @b: number of bytes or size memory to allocate
  *
- * Return: return an address or pointer to allocated memory or termination if error
+ * Return: return address or pointer to allocated memory or termination if err
  */
 
 void *malloc_checked(unsigned int b)
