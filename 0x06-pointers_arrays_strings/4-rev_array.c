079308aef0c91b33820156e9cf0805adc20cce52
Tesh2714 <teas38mum@gmail.com>
2022-04-10 21:44:51 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,27 +2,26 @@
 #include <stdio.h>
 
 /**
- *  * main - check the code
- *   * @a: an array of integers
- *    * @n: the number of elements to swap
- *     *
- *      * Return: nothing.
- *       */
+ * main - check the code
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ * Return: nothing.
+ */
 
 void reverse_array(int *a, int n)
 {
 	int temp, last, i;
+
 	last = n - 1;
 	i = 0;
 
-		while (i < last)
-		{
-
-				temp = *(a + i);
-				*(a + i) = *(a + last);
-				*(a + last) = temp;
-				i++;
-				last--;
-		}
+	while (i < last)
+	{
+		temp = *(a + i);
+		*(a + i) = *(a + last);
+		*(a + last) = temp;
+		i++;
+		last--;
+	}
 }
 
